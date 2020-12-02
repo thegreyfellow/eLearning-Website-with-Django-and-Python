f10222c8cdb994568b265d806e9bad13d512a043
Adil Hemache <adilhemache@gmail.com>
2020-12-02 21:25:23 +0100

    [refactor] Pretty URLs

--- courses/urls.py
+++ courses/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.course_list, name='course_list'),
     url(r'(?P<course_pk>\d+)/t(?P<step_pk>\d+)/$', views.text_detail, name='text_detail'),
-    url(r'(?P<course_pk>\d+)/q(?P<step_pk>\d+)/$', views.quiz_detail, name='quiz_detail'),
+    url(r'(?P<course_pk>\d+)/q/(?P<step_pk>\d+)/$', views.quiz_detail, name='quiz_detail'),
     url(r'(?P<course_pk>\d+)/create_quiz/$', views.quiz_create, name='create_quiz'),
     url(r'(?P<course_pk>\d+)/edit_quiz(?P<quiz_pk>\d+)/$', views.quiz_edit, name='edit_quiz'),
     url(r'(?P<quiz_pk>\d+)/create_question/$', views.create_question, name='create_question'),
